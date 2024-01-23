f6c5f2765f2bb3cf9b1bae4f917834a87c8f5e64
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2024-01-23 17:54:21 +0100

    soc: nordic: Add initial support for nRF54H20 EngA
    Add nrfx and Kconfig related infrastructure plus SoC initialization
    code to allow building for nRF54H20 targets (Application and Radio
    cores).
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

---
+++ soc/arm/nordic_nrf/nrf54h/soc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _NORDICSEMI_NRF54H_SOC_H_
+#define _NORDICSEMI_NRF54H_SOC_H_
+
+#include <soc_nrf_common.h>
+
+#endif /* _NORDICSEMI_NRF54H_SOC_H_ */
