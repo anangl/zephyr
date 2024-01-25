1d50619618bfaff1bcad3d6268095d74d9ff2806
Gerard Marull-Paretas <gerard@teslabs.com>
2024-01-25 10:35:52 +0100

    soc: arm: nordic_nrf: nrf54h: pull HSFLL configuration from DT
    Instead of Kconfig options.
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- soc/arm/nordic_nrf/nrf54h/soc.c
+++ soc/arm/nordic_nrf/nrf54h/soc.c
@@ -5,6 +5,7 @@
  */
 
 #include <zephyr/cache.h>
+#include <zephyr/devicetree.h>
 #include <zephyr/init.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
@@ -15,13 +16,15 @@ LOG_MODULE_REGISTER(soc, CONFIG_SOC_LOG_LEVEL);
 
 #if defined(NRF_APPLICATION)
 #define HSFLL_NODE DT_NODELABEL(cpuapp_hsfll)
-#define FICR_TRIM_BLOCK NRF_FICR->TRIM.APPLICATION
 #elif defined(NRF_RADIOCORE)
 #define HSFLL_NODE DT_NODELABEL(cpurad_hsfll)
-#define FICR_TRIM_BLOCK NRF_FICR->TRIM.RADIOCORE
 #else
 #error "Unsupported SoC"
 #endif
+
+#define FICR_ADDR_GET(node_id, name)                                           \
+	DT_REG_ADDR(DT_PHANDLE_BY_NAME(node_id, nordic_ficrs, name)) +         \
+		DT_PHA_BY_NAME(node_id, nordic_ficrs, name, offset)
 
 static void power_domain_init(void)
 {
@@ -49,23 +52,22 @@ static void power_domain_init(void)
 #endif
 }
 
-#if defined(CONFIG_SOC_TRIM_HSFLL)
 static int trim_hsfll(void)
 {
 	NRF_HSFLL_Type *hsfll = (NRF_HSFLL_Type *)DT_REG_ADDR(HSFLL_NODE);
-	const nrf_hsfll_trim_t trim = {
-		.vsup = FICR_TRIM_BLOCK.HSFLL.TRIM.VSUP,
-		.coarse = FICR_TRIM_BLOCK.HSFLL.TRIM.COARSE[CONFIG_FICR_HSFLL_TRIM_IDX],
-		.fine = FICR_TRIM_BLOCK.HSFLL.TRIM.FINE[CONFIG_FICR_HSFLL_TRIM_IDX],
+	nrf_hsfll_trim_t trim = {
+		.vsup = sys_read32(FICR_ADDR_GET(HSFLL_NODE, vsup)),
+		.coarse = sys_read32(FICR_ADDR_GET(HSFLL_NODE, coarse)),
+		.fine = sys_read32(FICR_ADDR_GET(HSFLL_NODE, fine))
 	};
 
 	LOG_DBG("Trim: HSFLL VSUP: 0x%.8x", trim.vsup);
 	LOG_DBG("Trim: HSFLL COARSE: 0x%.8x", trim.coarse);
 	LOG_DBG("Trim: HSFLL FINE: 0x%.8x", trim.fine);
 
-	if (CONFIG_HSFLL_CLOCK_MULTIPLIER) {
-		nrf_hsfll_clkctrl_mult_set(hsfll, CONFIG_HSFLL_CLOCK_MULTIPLIER);
-	}
+	nrf_hsfll_clkctrl_mult_set(hsfll,
+				   DT_PROP(HSFLL_NODE, clock_frequency) /
+					   DT_PROP(DT_CLOCKS_CTLR(HSFLL_NODE), clock_frequency));
 	nrf_hsfll_trim_set(hsfll, &trim);
 
 	nrf_hsfll_task_trigger(hsfll, NRF_HSFLL_TASK_FREQ_CHANGE);
@@ -82,7 +84,6 @@ static int trim_hsfll(void)
 
 	return 0;
 }
-#endif /* CONFIG_SOC_TRIM_HSFLL */
 
 static int nordicsemi_nrf54h_init(void)
 {
@@ -92,9 +93,7 @@ static int nordicsemi_nrf54h_init(void)
 
 	power_domain_init();
 
-#if defined(CONFIG_SOC_TRIM_HSFLL)
 	trim_hsfll();
-#endif
 
 	return 0;
 }
